2c2cb3e101ff8f8646f84744d28f9b44ed2d9680
Kev <87592672+0xkev21@users.noreply.github.com>
2024-01-19 23:37:05 +0630

    Change the Book Factory Function into a Class

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-Book.prototype.changeStatus = function() {
-    this.read = this.read ? false : true;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    changeStatus () {
+        this.read = this.read ? false : true;
+    }
 }
 
 function addBook(title, author, pages, read) {
